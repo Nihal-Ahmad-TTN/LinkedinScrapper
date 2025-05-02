156cfb126f37253e6992ddc19f015524a9999e8a
Nihal Ahmad <nihal.ahmad@tothenew.com>
2025-05-02 12:15:27 +0530

    V5

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ import os
 import config
 
 
-linkedin = LinkedInScraper()
+scraper = LinkedInScraper()
 os.system("clear")
 print("====================== WELCOME TO LINKED IN SCRAPPER =====================================\n\n\n")
-linkedin.email = config.LINKEDIN_EMAIL
-linkedin.password =config.LINKEDIN_PASSWORD
-linkedin.company = config.COMPANY
-linkedin.number=config.NUMBER_OF_PROFILES
+scraper.email = config.LINKEDIN_EMAIL
+scraper.password = config.LINKEDIN_PASSWORD
+scraper.company = config.COMPANY
+scraper.number = config.NUMBER_OF_PROFILES
 
 
-while linkedin.number >100 and linkedin.number<=0:
+while scraper.number > 100 and scraper.number <= 0:
     print("Incorrect number is given please give the correct number")
-    linkedin.number=int(input("Enter the total number of profile you want to scrape, note that the numb er should not exceed 100"))
+    scraper.number = int(input("Enter the total number of profile you want to scrape, note that the numb er should not exceed 100"))
 
-linkedin.search_query = config.SEARCH_QUERY
-linkedin.COOKIE_FILE = config.COOKIE_FILE
+scraper.search_query = config.SEARCH_QUERY
+scraper.COOKIE_FILE = config.COOKIE_FILE
 
 print(f"Scrapping for the following details- Company: {config.COMPANY}\nNumber:{config.NUMBER_OF_PROFILES}\nSearh Query:{ config.SEARCH_QUERY}")
-linkedin.scraper()
+scraper.scraper()
